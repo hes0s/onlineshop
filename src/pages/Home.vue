96de999e642b412e04ff390a1d7c8e6eda4a3ef7
hes0sss <cusnircristi161@gmail.com>
2025-03-17 14:27:01 +0200

    hello

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <h1 class="text-3xl underline text-blue-600 bg-gray-200">HomePage</h1>
     <a class="text-blue-600 bg-red-200 p-2 m-4" href="https://github.com/N1krat/onlineshop">Github</a>
+    Hello world
 </template>
 
 <script>
