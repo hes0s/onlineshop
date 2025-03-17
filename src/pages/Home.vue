910a567426877f7fd8b36aa297cec418f3608dd7
N1krat <bogdanbraniste2007@gmail.com>
2025-03-17 11:23:41 +0200

    #4 Created file system with some files in them

---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="text-3xl underline text-blue-600 bg-gray-200">HomePage</h1>
+    <a class="text-blue-600 bg-red-200 p-2 m-4" href="https://github.com/N1krat/onlineshop">Github</a>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+}
+</script>
+
+<style>
+
+</style>
