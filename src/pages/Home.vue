0cb30d2098e4981dc58f137c1a714ecdf0801701
N1krat <bogdanbraniste2007@gmail.com>
2025-03-17 14:33:13 +0200

    gf

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <h1 class="text-3xl underline text-blue-600 bg-gray-200">HomePage</h1>
-    <a class="text-blue-600 bg-red-200 p-2 m-4" href="https://github.com/N1krat/onlineshop">Github</a>
+  <a href="https://github.com/N1krat/onlineshop" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">GitHub</a>
 </template>
 
 <script>
